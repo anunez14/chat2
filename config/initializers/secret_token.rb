940ce684a9c4bbf7a6e99c56962c227aa33e9a8a
unknown <VAIO@VAIO-VAIO.(none)>
2012-02-01 01:55:23 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fulbitofacil::Application.config.secret_token = '1fb27d1de59f777a1746cef4c9bf432b4fb428ba179077d17332a7549ed539f740aadd5e8e50af0b005af1e48209f9258fe5ed1c9ea7f76b5deed8af51a6d1ff'
